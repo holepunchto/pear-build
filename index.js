c718b845e0a46f27dcc9c8c8faa6870dfe5b84bc
Andrei Regiani <andrei.cpp@gmail.com>
2025-10-02 02:40:55 -0300

    refactor

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 'use strict'
 const fs = require('bare-fs')
 const path = require('bare-path')
-const dump = require('pear-dump')
 const make = require('bare-make')
-const { spawnSync } = require('bare-subprocess')
-const { arch, platform } = require('which-runtime')
+const dump = require('pear-dump')
+
+async function cpRecursive (src, dest) {
+  // @TODO: replace with bare-fs once it supports `fs.cpSync { recursive: true }`
+  await fs.promises.mkdir(dest, { recursive: true })
+  const entries = await fs.promises.readdir(src, { withFileTypes: true })
+  for (const entry of entries) {
+    const from = path.join(src, entry.name)
+    const to = path.join(dest, entry.name)
+    if (entry.isDirectory()) {
+      await cpRecursive(from, to)
+    } else if (entry.isSymbolicLink && entry.isSymbolicLink()) {
+      const target = await fs.promises.readlink(from)
+      await fs.promises.symlink(target, to)
+    } else {
+      await fs.promises.copyFile(from, to)
+    }
+  }
+}
 
 module.exports = async function build ({ link, dir }) {
-    const repoDir = dir + '/pear-appling'
-    const dumpDir = dir + '/pear-dump'
+    // const output = new Readable({ objectMode: true })
+    const distributables = path.join(dir, 'distributables')
+    console.log(`build ${link} -> ${distributables}`)
 
-    if (!fs.existsSync(repoDir)) {
-      const resultGit = spawnSync('git', ['clone', 'https://github.com/holepunchto/pear-appling'], { cwd: dir, stdio: 'inherit' })
-      if (resultGit.status !== 0) throw new Error(`git error ${resultGit.stderr?.toString()}`)
-    }
+    await fs.promises.mkdir(distributables, { recursive: true })
 
-    console.log('Installing npm packages...')
-    const resultNpm = spawnSync('npm', ['i'], { cwd: repoDir, stdio: 'inherit' })
-    if (resultNpm.status !== 0) throw new Error(`npm error ${resultNpm.stderr?.toString()}`)
-
-    const stream = dump(link, { dir: dumpDir, list: ['package.json'] })
+    const stream = await dump(link, { dir: distributables, list: ['package.json', 'appling-assets'] })
     await new Promise(resolve => stream.once('end', resolve))
 
-    let pkgBuf = await fs.promises.readFile(dumpDir + '/package.json')
-    if (pkgBuf && pkgBuf.value) pkgBuf = pkgBuf.value
-    const pkg = JSON.parse(Buffer.from(pkgBuf).toString())
-    const build = pkg?.pear?.build
-    if (!build) throw Error('package.json is missing field pear.build')
+    const cmakePear = path.join(Pear.config.pearDir, 'current', 'node_modules', 'cmake-pear')
+    await cpRecursive(cmakePear, path.join(distributables, 'node_modules', 'cmake-pear'))
+    const pkgJson = await import(path.join(distributables, 'package.json'), { assert: { type: 'json' } })
+    const build = pkgJson.default.pear.build
 
     const cmakeTxt = `
     cmake_minimum_required(VERSION 3.31)
@@ -37,31 +46,23 @@ module.exports = async function build ({ link, dir }) {
       ID "${build.id}"
       NAME "${build.name}"
       LINK "${build.link}"
-      VERSION ${build.version}
+      VERSION "${build.version}"
       AUTHOR "${build.author}"
       DESCRIPTION "${build.description}"
       MACOS_IDENTIFIER "${build.darwin.identifier}"
       MACOS_CATEGORY "${build.darwin.category}"
-      MACOS_SIGNING_IDENTITY "${build.darwin.signing-identity}"
-      WINDOWS_SIGNING_SUBJECT "${build.win32.signing-subject}"
-      WINDOWS_SIGNING_THUMBPRINT "${build.win32.signing-thumbprint}"
+      MACOS_SIGNING_IDENTITY "${build.darwin['signing-identity']}"
+      WINDOWS_SIGNING_SUBJECT "${build.win32['signing-subject']}"
+      WINDOWS_SIGNING_THUMBPRINT "${build.win32['signing-thumbprint']}"
       LINUX_CATEGORY "${build.linux.category}"
     )`
-    await fs.promises.writeFile(repoDir + '/CMakeLists.txt', cmakeTxt)
+    await fs.promises.writeFile(path.join(distributables, 'CMakeLists.txt'), cmakeTxt)
 
     console.log('bare-make generate...')
-    await make.generate({ cwd: repoDir })
+    await make.generate({ cwd: distributables })
 
     console.log('bare-make build...')
-    await make.build({ cwd: repoDir })
+    await make.build({ cwd: distributables })
 
-    console.log('Cleaning up...')
-    const srcDir = path.join(repoDir, 'build')
-    const dstDir = path.join(dir, 'distributables', `${platform}-${arch}`)
-    await fs.promises.mkdir(dstDir, { recursive: true })
-    await fs.promises.cp(srcDir, dstDir, { recursive: true })
-    await fs.promises.rm(repoDir, { recursive: true, force: true })
-    await fs.promises.rm(dumpDir, { recursive: true, force: true })
-
-    console.log(`Built appling at ${dstDir}`)
+    console.log(`Built appling at ${distributables}`)
 }
