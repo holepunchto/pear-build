6b7694bae1d5367393ea6242934378a2173f32f4
Andrei Regiani <andrei.cpp@gmail.com>
2025-10-02 03:28:50 -0300

    streamx, pear-link

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const fs = require('bare-fs')
 const path = require('bare-path')
 const make = require('bare-make')
 const dump = require('pear-dump')
+const plink = require('pear-link')
+const { Readable } = require('streamx')
 
 async function cpRecursive (src, dest) {
-  // @TODO: replace with bare-fs once it supports `fs.cpSync { recursive: true }`
+  // @TODO: replace this with bare-fs once supports `fs.promises.cp { recursive: true }`
   await fs.promises.mkdir(dest, { recursive: true })
   const entries = await fs.promises.readdir(src, { withFileTypes: true })
   for (const entry of entries) {
@@ -23,19 +25,19 @@ async function cpRecursive (src, dest) {
 }
 
 module.exports = async function build ({ link, dir }) {
-    // const output = new Readable({ objectMode: true })
+    const output = new Readable({ objectMode: true })
     const distributables = path.join(dir, 'distributables')
-    console.log(`build ${link} -> ${distributables}`)
+    output.push({ tag: 'init', data: { dir: distributables } })
 
     await fs.promises.mkdir(distributables, { recursive: true })
-
-    const stream = await dump(link, { dir: distributables, list: ['package.json', 'appling-assets'] })
+    const stream = await dump(link, { dir: distributables, list: ['package.json', 'assets'] })
     await new Promise(resolve => stream.once('end', resolve))
 
     const cmakePear = path.join(Pear.config.pearDir, 'current', 'node_modules', 'cmake-pear')
     await cpRecursive(cmakePear, path.join(distributables, 'node_modules', 'cmake-pear'))
     const pkgJson = await import(path.join(distributables, 'package.json'), { assert: { type: 'json' } })
     const build = pkgJson.default.pear.build
+    const { drive } = plink.parse(link)
 
     const cmakeTxt = `
     cmake_minimum_required(VERSION 3.31)
@@ -43,9 +45,9 @@ module.exports = async function build ({ link, dir }) {
     project(pear_appling C CXX ASM)
     add_pear_appling(
       pear_appling
-      ID "${build.id}"
+      ID "${drive.key}"
       NAME "${build.name}"
-      LINK "${build.link}"
+      LINK "${link}"
       VERSION "${build.version}"
       AUTHOR "${build.author}"
       DESCRIPTION "${build.description}"
@@ -58,11 +60,11 @@ module.exports = async function build ({ link, dir }) {
     )`
     await fs.promises.writeFile(path.join(distributables, 'CMakeLists.txt'), cmakeTxt)
 
-    console.log('bare-make generate...')
+    output.push({ tag: 'generate' })
     await make.generate({ cwd: distributables })
 
-    console.log('bare-make build...')
+    output.push({ tag: 'build' })
     await make.build({ cwd: distributables })
 
-    console.log(`Built appling at ${distributables}`)
+    output.push({ tag: 'complete', data: { dir: distributables } })
 }
