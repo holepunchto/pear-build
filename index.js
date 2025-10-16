bf6e2271b008af0b56a53a1ad04cde1ba33ed562
Andrei Regiani <andrei.cpp@gmail.com>
2025-10-16 01:35:24 -0300

    use npm

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 const path = require('bare-path')
 const make = require('bare-make')
 const { arch, platform } = require('which-runtime')
+const { spawnSync } = require('bare-subprocess')
 const { Readable } = require('streamx')
 
-module.exports = async function build ({ link, dir }) {
+module.exports = async function build ({ dir }) {
     const output = new Readable({ objectMode: true })
     const host = platform + '-' + arch
     const distributables = path.join(dir, 'distributables', host)
     output.push({ tag: 'init', data: { distributables } })
+
+    output.push({ tag: 'npm' })
+    const result = spawnSync('npm', ['i', 'cmake-pear', '--no-package-lock', '--no-save'], { cwd: distributables, stdio: 'inherit' })
+    if (result.status !== 0) throw new Error(`npm error ${result.stderr?.toString()}`)
 
     output.push({ tag: 'generate' })
     await make.generate({ cwd: distributables })
