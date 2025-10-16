c214d1b56e8f3d9e151c5dc633f1b413d63f1e31
Andrei Regiani <andrei.cpp@gmail.com>
2025-10-16 09:21:12 -0300

    npm cmake-pear

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ const { Readable } = require('streamx')
 
 module.exports = async function build ({ dir }) {
     const output = new Readable({ objectMode: true })
-    const host = platform + '-' + arch
-    const distributables = path.join(dir, 'distributables')
+    const distributables = path.join(dir, 'distributables', platform + '-' + arch)
     output.push({ tag: 'init', data: { distributables } })
 
     output.push({ tag: 'npm' })
-    const result = spawnSync('npm', ['i'], { cwd: distributables, stdio: 'inherit' })
-    if (result.status !== 0) throw new Error(`npm error ${result.stderr?.toString()}`)
+    const result = spawnSync('npm', ['i', 'cmake-pear', '--no-save', '--no-package-lock', '--force'], { cwd: distributables, stdio: 'inherit' })
+    if (result.status !== 0) throw new Error('npm install failed')
 
     output.push({ tag: 'generate' })
     await make.generate({ cwd: distributables })
