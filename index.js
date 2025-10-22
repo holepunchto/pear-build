7b95b87e738f5ba72486ab0c1794b4d6b73544e8
Andrei Regiani <andrei.cpp@gmail.com>
2025-10-22 20:05:53 -0300

    remove npm spawn dependency

--- index.js
+++ index.js
@@ -2,17 +2,12 @@
 const path = require('bare-path')
 const make = require('bare-make')
 const { arch, platform } = require('which-runtime')
-const { spawnSync } = require('bare-subprocess')
 const { Readable } = require('streamx')
 
 module.exports = async function build ({ dir }) {
     const output = new Readable({ objectMode: true })
     const distributables = path.join(dir, 'distributables', platform + '-' + arch)
     output.push({ tag: 'init', data: { distributables } })
-
-    output.push({ tag: 'npm' })
-    const result = spawnSync('npm', ['i', 'cmake-pear', '--prefix', '.'], { cwd: distributables, stdio: 'inherit' })
-    if (result.status !== 0) throw new Error('npm install failed')
 
     output.push({ tag: 'generate' })
     await make.generate({ cwd: distributables })
