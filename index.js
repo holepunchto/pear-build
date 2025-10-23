c18a6f8b259c15c9b3eac26d7ff8738a031c5d4f
Andrei Regiani <andrei.cpp@gmail.com>
2025-10-23 02:09:20 -0300

    clean up

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict'
-const path = require('bare-path')
 const make = require('bare-make')
-const { arch, platform } = require('which-runtime')
 const { Readable } = require('streamx')
 
 module.exports = async function build ({ dir }) {
     const output = new Readable({ objectMode: true })
-    const distributables = path.join(dir, 'distributables', platform + '-' + arch)
-    output.push({ tag: 'init', data: { distributables } })
+    output.push({ tag: 'init', data: { dir } })
 
     output.push({ tag: 'generate' })
-    await make.generate({ cwd: distributables })
+    await make.generate({ cwd: dir })
 
     output.push({ tag: 'build' })
-    await make.build({ cwd: distributables })
+    await make.build({ cwd: dir })
 
-    output.push({ tag: 'complete', data: { dir: distributables } })
+    output.push({ tag: 'complete', data: { dir } })
+    output.push({ tag: 'final', data: { success: true } })
+    output.push(null)
+    return output
 }
