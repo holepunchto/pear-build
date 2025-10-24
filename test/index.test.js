ed451bd8416aabb31632aaafad587a42d1990875
Andrei Regiani <andrei.cpp@gmail.com>
2025-10-24 13:19:01 -0300

    update test

--- test/index.test.js
+++ test/index.test.js
@@ -5,10 +5,14 @@ const pearBuild = require('..')
 const fixtureDir = path.resolve(__dirname, 'fixtures', 'distributables')
 
 test('build({ dir })', async t => {
-  t.plan(1)
+  t.plan(5)
   const dir = fixtureDir
   const stream = await pearBuild({ dir })
   stream.on('data', (msg) => {
+    t.alike(msg, { tag: 'init', data: { dir: fixtureDir } })
+    t.alike(msg, { tag: 'generate' })
+    t.alike(msg, { tag: 'build' })
     t.alike(msg, { tag: 'complete', data: { dir: fixtureDir } })
+    t.alike(msg, { tag: 'final', data: { success: true } })
   })
 })
