344ae2752b5b6f46bdb8d5895b6b997ea7e998f1
Andrei Regiani <andrei.cpp@gmail.com>
2025-10-28 13:38:11 -0300

    _build refactor, fix tests logic

--- index.js
+++ index.js
@@ -2,23 +2,25 @@
 const make = require('bare-make')
 const { Readable } = require('streamx')
 
-module.exports = async function build ({ dir }) {
+module.exports = function build ({ dir }) {
   const output = new Readable({ objectMode: true })
-  setImmediate(async () => {
-    try {
-      output.push({ tag: 'init', data: { dir } })
-      output.push({ tag: 'generate' })
-      await make.generate({ cwd: dir })
-      output.push({ tag: 'build' })
-      await make.build({ cwd: dir })
-      output.push({ tag: 'complete', data: { dir } })
-      output.push({ tag: 'final', data: { success: true } })
-    } catch (err) {
-      output.push({ tag: 'error', data: { message: err.message, stack: err.stack } })
-      output.push({ tag: 'final', data: { success: false, message: err.message } })
-    } finally {
-      output.push(null)
-    }
-  })
+  _build(output, { dir })
   return output
+}
+
+async function _build (output, { dir }) {
+  try {
+    output.push({ tag: 'init', data: { dir } })
+    output.push({ tag: 'generate' })
+    await make.generate({ cwd: dir })
+    output.push({ tag: 'build' })
+    await make.build({ cwd: dir })
+    output.push({ tag: 'complete', data: { dir } })
+    output.push({ tag: 'final', data: { success: true } })
+  } catch (err) {
+    output.push({ tag: 'error', data: { message: err.message, stack: err.stack } })
+    output.push({ tag: 'final', data: { success: false, message: err.message } })
+  } finally {
+    output.push(null)
+  }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -4,15 +4,14 @@ const path = require('bare-path')
 const pearBuild = require('..')
 const fixtureDir = path.resolve(__dirname, 'fixtures', 'distributables')
 
-test('build({ dir })', async t => {
-  t.plan(5)
+test('build({ dir })', async function ({ plan, alike, timeout }) {
+  timeout(1_800_000) // 30 minutes
+  plan(5)
   const dir = fixtureDir
-  const stream = await pearBuild({ dir })
-  stream.on('data', (msg) => {
-    t.alike(msg, { tag: 'init', data: { dir: fixtureDir } })
-    t.alike(msg, { tag: 'generate' })
-    t.alike(msg, { tag: 'build' })
-    t.alike(msg, { tag: 'complete', data: { dir: fixtureDir } })
-    t.alike(msg, { tag: 'final', data: { success: true } })
-  })
+  const stream = pearBuild({ dir })
+  alike(await new Promise(r => stream.once('data', r)), { tag: 'init', data: { dir: fixtureDir } })
+  alike(await new Promise(r => stream.once('data', r)), { tag: 'generate' })
+  alike(await new Promise(r => stream.once('data', r)), { tag: 'build' })
+  alike(await new Promise(r => stream.once('data', r)), { tag: 'complete', data: { dir: fixtureDir } })
+  alike(await new Promise(r => stream.once('data', r)), { tag: 'final', data: { success: true } })
 })
