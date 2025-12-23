7249bf189219083cb6d68748809ac4aceb1f0278
Andrei Regiani <andrei.cpp@gmail.com>
2025-12-23 10:40:03 -0300

    improve examples

--- examples/bare-build.mjs
+++ examples/bare-build.mjs
@@ -2,10 +2,14 @@ import bareBuild from 'bare-build'
 import path from 'bare-path'
 import { arch, platform } from 'which-runtime'
 
-const entry = path.join('test', 'fixtures', 'runtime', '.pear', 'appling', 'app.cjs')
+const base = path.join('test', 'fixtures', 'runtime', '.pear', 'appling')
+const entry = path.join(base, 'app.cjs')
 const opts = {
+  name: 'Runtime',
   target: [`${platform}-${arch}`],
-  package: true,
+  package: false,
+  standalone: false,
+  base,
   out: '.'
 }
 for await (const resource of bareBuild(entry, opts)) {
--- examples/pear-build.mjs
+++ examples/pear-build.mjs
@@ -2,8 +2,7 @@ import pearBuild from '..'
 import path from 'bare-path'
 import { fileURLToPath } from 'bare-url'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const dotPear = path.resolve(__dirname, '..', 'test', 'fixtures', 'runtime', '.pear')
 const stream = await pearBuild({ dotPear })
 stream.on('data', (info) => console.log(info))
