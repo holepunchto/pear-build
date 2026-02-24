8cf1a33ac2763a2127e66a7c668fad50fa521fb7
Geordan Gesink <georgegeordan@gmail.com>
2026-02-24 18:10:28 +0100

    add mobile support

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ const program = command(
   flag('--linux-arm64-app [path]', 'Path to Linux ARM64 app'),
   flag('--linux-x64-app [path]', 'Path to Linux x64 app'),
   flag('--win32-x64-app [path]', 'Path to Windows x64 app'),
-  flag('--target [path]', 'Target build dir'),
+  flag('--ios-arm64 [path]', 'Path to iOS ARM64 folder (ota bundle and assets)'),
+  flag('--ios-arm64-simulator [path]', 'Path to iOS ARM64-Simulator folder (ota bundle and assets)'),
+  flag('--ios-x64-simulator [path]', 'Path to iOS x64-Simulator folder (ota bundle and assets)'),
+  flag('--android-arm64 [path]', 'Path to android ARM64 folder (ota bundle and assets)'),
   async function (cmd) {
     const package = path.resolve(cmd.flags.package)
     const pkg = require(package)
+    const productName = pkg.productName
     const { target = path.resolve(pkg.name + '-' + pkg.version) } = cmd.flags
     const darwinArm64App = cmd.flags.darwinArm64App
       ? ['darwin-arm64', path.resolve(cmd.flags.darwinArm64App)]
@@ -32,6 +36,16 @@ const program = command(
     const win32X64App = cmd.flags.win32X64App
       ? ['win32-x64', path.resolve(cmd.flags.win32X64App)]
       : null
+    const iosArm64 = cmd.flags.iosArm64 ? ['ios-arm64', path.resolve(cmd.flags.iosArm64)] : null
+    const iosArm64Sim = cmd.flags.iosArm64Sim
+      ? ['ios-arm64-sim', path.resolve(cmd.flags.iosArm64Sim)]
+      : null
+    const iosx64Sim = cmd.flags.iosx64Sim
+      ? ['ios-x64-sim', path.resolve(cmd.flags.iosx64Sim)]
+      : null
+    const androidArm64 = cmd.flags.win32X64App
+      ? ['android-arm64', path.resolve(cmd.flags.androidArm64)]
+      : null
 
     const byArch = path.join(target, 'by-arch')
 
@@ -39,13 +53,24 @@ const program = command(
 
     fs.writeFileSync(path.join(target, 'package.json'), fs.readFileSync(package))
 
-    const apps = [darwinArm64App, darwinX64App, linuxArm64App, linuxX64App, win32X64App].filter(
-      Boolean
-    )
+    const apps = [
+      darwinArm64App,
+      darwinX64App,
+      linuxArm64App,
+      linuxX64App,
+      win32X64App,
+      iosArm64,
+      iosArm64Sim,
+      iosx64Sim,
+      androidArm64
+    ].filter(Boolean)
+
     const noop = () => {}
     const promises = []
     for (const [arch, app] of apps) {
-      const archApp = path.join(byArch, arch, 'app', path.basename(app))
+      const isMobile = arch.startsWith('ios') || arch.startsWith('android')
+      if (isMobile && !productName ) throw new Error('productName field in package.json required')
+      const archApp = path.join(byArch, arch, 'app', isMobile ? productName : path.basename(app))
       await fs.promises.mkdir(archApp, { recursive: true })
       const src = new Localdrive(app)
       const dst = new Localdrive(archApp)
