39d4b2a1ded86ed145a78e210b4fa68f139d32b4
Geordan Gesink <165498764+geordangesink@users.noreply.github.com>
2026-02-25 15:18:33 +0100

    add mobile support (#6)
    * add mobile support
    
    * productName || name
    
    * bring back target flag
    
    * specify throw
    
    * add test script
    
    * rewording
    
    * clean; less specific error
    
    * change Error message
    
    * lint
    
    * adjust props
    
    * final adjustments and fixes

--- bin.js
+++ bin.js
@@ -8,22 +8,33 @@ const cmd = command(
   summary(pkg.description),
   flag('--version|-v', 'Print the current version'),
   flag('--package [path]', 'Path to project package.json'),
+  flag('--target [path]', 'Target build dir'),
   flag('--darwin-arm64-app [path]', 'Path to Mac ARM64 app'),
   flag('--darwin-x64-app [path]', 'Path to Mac x64 app'),
   flag('--linux-arm64-app [path]', 'Path to Linux ARM64 app'),
   flag('--linux-x64-app [path]', 'Path to Linux x64 app'),
   flag('--win32-x64-app [path]', 'Path to Windows x64 app'),
-  flag('--target [path]', 'Target build dir'),
+  flag('--ios-arm64 [path]', 'Path to iOS ARM64 folder (ota bundle and assets)'),
+  flag(
+    '--ios-arm64-simulator [path]',
+    'Path to iOS ARM64-Simulator folder (ota bundle and assets)'
+  ),
+  flag('--ios-x64-simulator [path]', 'Path to iOS x64-Simulator folder (ota bundle and assets)'),
+  flag('--android-arm64 [path]', 'Path to android ARM64 folder (ota bundle and assets)'),
   async function (cmd) {
     if (cmd.flags.version) return console.log(`v${pkg.version}`)
     const dir = cmd.flags.package
     const opts = {
+      target: cmd.flags.target,
       darwinArm64App: cmd.flags.darwinArm64App,
       darwinX64App: cmd.flags.darwinX64App,
       linuxArm64App: cmd.flags.linuxArm64App,
       linuxX64App: cmd.flags.linuxX64App,
       win32X64App: cmd.flags.win32X64App,
-      target: cmd.flags.target
+      iosArm64: cmd.flags.iosArm64,
+      iosArm64Simulator: cmd.flags.iosArm64Simulator,
+      iosX64Simulator: cmd.flags.iosX64Simulator,
+      androidArm64: cmd.flags.androidArm64
     }
     try {
       await build(dir, opts)
--- index.js
+++ index.js
@@ -15,6 +15,14 @@ module.exports = async function build(dir, opts = {}) {
     : null
   const linuxX64App = opts.linuxX64App ? ['linux-x64', path.resolve(opts.linuxX64App)] : null
   const win32X64App = opts.win32X64App ? ['win32-x64', path.resolve(opts.win32X64App)] : null
+  const iosArm64 = opts.iosArm64 ? ['ios-arm64', path.resolve(opts.iosArm64)] : null
+  const iosArm64Sim = opts.iosArm64Simulator
+    ? ['ios-arm64-simulator', path.resolve(opts.iosArm64Simulator)]
+    : null
+  const iosx64Sim = opts.iosX64Simulator
+    ? ['ios-x64-simulator', path.resolve(opts.iosX64Simulator)]
+    : null
+  const androidArm64 = opts.androidArm64 ? ['android-arm64', path.resolve(opts.androidArm64)] : null
 
   const byArch = path.join(target, 'by-arch')
 
@@ -22,13 +30,27 @@ module.exports = async function build(dir, opts = {}) {
 
   fs.writeFileSync(path.join(target, 'package.json'), fs.readFileSync(pkgPath))
 
-  const apps = [darwinArm64App, darwinX64App, linuxArm64App, linuxX64App, win32X64App].filter(
-    Boolean
-  )
+  const apps = [
+    darwinArm64App,
+    darwinX64App,
+    linuxArm64App,
+    linuxX64App,
+    win32X64App,
+    iosArm64,
+    iosArm64Sim,
+    iosx64Sim,
+    androidArm64
+  ].filter(Boolean)
+
   const noop = () => {}
   const promises = []
   for (const [arch, app] of apps) {
-    const archApp = path.join(byArch, arch, 'app', path.basename(app))
+    const isMobile = arch.startsWith('ios') || arch.startsWith('android')
+    const appName = isMobile ? (pkg.productName ?? pkg.name) : path.basename(app)
+    if (typeof appName !== 'string') {
+      throw new Error('package.json productName or name is a required field string')
+    }
+    const archApp = path.join(byArch, arch, 'app', appName)
     await fs.promises.mkdir(archApp, { recursive: true })
     const src = new Localdrive(app)
     const dst = new Localdrive(archApp)
