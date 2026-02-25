976a6833548394a3cac3347ed582ed9cbc3d4770
Andrei Regiani <andrei.cpp@gmail.com>
2026-02-25 08:54:23 -0300

    bin.js refactor + CI workflow clean up (#9)
    * bin.js refactor
    
    * integrate.yml
    
    * build(dir, opts)

---
+++ bin.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+const { command, flag, summary } = require('paparam')
+const pkg = require('./package')
+const build = require('.')
+
+const cmd = command(
+  pkg.name,
+  summary(pkg.description),
+  flag('--version|-v', 'Print the current version'),
+  flag('--package [path]', 'Path to project package.json'),
+  flag('--darwin-arm64-app [path]', 'Path to Mac ARM64 app'),
+  flag('--darwin-x64-app [path]', 'Path to Mac x64 app'),
+  flag('--linux-arm64-app [path]', 'Path to Linux ARM64 app'),
+  flag('--linux-x64-app [path]', 'Path to Linux x64 app'),
+  flag('--win32-x64-app [path]', 'Path to Windows x64 app'),
+  flag('--target [path]', 'Target build dir'),
+  async function (cmd) {
+    if (cmd.flags.version) return console.log(`v${pkg.version}`)
+    const dir = cmd.flags.package
+    const opts = {
+      darwinArm64App: cmd.flags.darwinArm64App,
+      darwinX64App: cmd.flags.darwinX64App,
+      linuxArm64App: cmd.flags.linuxArm64App,
+      linuxX64App: cmd.flags.linuxX64App,
+      win32X64App: cmd.flags.win32X64App,
+      target: cmd.flags.target
+    }
+    try {
+      await build(dir, opts)
+    } catch (err) {
+      if (err) console.error(err)
+      if (typeof process !== 'undefined') process.exitCode = 1
+    }
+  }
+)
+
+cmd.parse()
