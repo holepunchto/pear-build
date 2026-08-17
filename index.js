1a7270c6726a1f30cb542cee5e9b5b1dcf76ecfc
Geordan Gesink <georgegeordan@gmail.com>
2026-08-17 14:14:40 +0200

    bolean

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class Build extends EventEmitter {
     const pkgPath = path.resolve(opts.package)
     const pkg = await getParsedJSON('package.json', pkgPath)
 
-    const isMobile = pkg.dependencies?.['react-native-bare-kit'] // in mobile react-native-bare-kit needs to be listed in project's deps
+    const isMobile = !!pkg.dependencies?.['react-native-bare-kit'] // in mobile react-native-bare-kit needs to be listed in project's deps
     if (!opts.config && isMobile) throw ERR_INVALID_INPUT('pear.json path must be specified.')
     const configPath = opts.config && path.resolve(opts.config)
     const config = configPath && (await getParsedJSON('pear.json', configPath))
