b876378cada6daab9cc66925959ccf204aeeb201
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-09-22 18:06:02 +0800

    Add building emit

--- bin.js
+++ bin.js
@@ -7,6 +7,9 @@ const cmd = command(pkg.name, pkg.command, async function (cmd) {
   if (cmd.flags.version) return console.log(`v${pkg.version}`)
   try {
     const runner = build(cmd.flags)
+    runner.on('building', ({ message, pkg }) =>
+      console.log(`${message} ${pkg.name} (v${pkg.version})...`)
+    )
     runner.on('mirrored', (data) => console.log(data.from, data.message, data.to))
     runner.on('error', (err) => console.error(err))
     await runner.done()
